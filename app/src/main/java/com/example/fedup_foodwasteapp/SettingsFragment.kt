68149d7c4a3d31e16606f53b1bc0ac0770f02cce
trent <trentfisher017@gmail.com>
2024-08-24 16:58:10 +0200

    Latest project

--- app/src/main/java/com/example/fedup_foodwasteapp/SettingsFragment.kt
+++ app/src/main/java/com/example/fedup_foodwasteapp/SettingsFragment.kt
@@ -1,6 +1,10 @@
 package com.example.fedup_foodwasteapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -8,4 +12,14 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+        return view
+    }
+
 }
