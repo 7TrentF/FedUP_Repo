2665abd0e98b16b1432488185e9f1002dcd3601e
trent <trentfisher017@gmail.com>
2024-10-02 19:05:41 +0200

    re-published REST Api

--- app/src/main/java/com/example/fedup_foodwasteapp/RetrofitClient.kt
+++ app/src/main/java/com/example/fedup_foodwasteapp/RetrofitClient.kt
@@ -7,7 +7,7 @@ import android.util.Log
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://fedupmanagementapi20240925105410.azurewebsites.net/"
+    private const val BASE_URL = "https://fedupapi20240922180228.azurewebsites.net/"
 
 
     private val retrofit by lazy {
